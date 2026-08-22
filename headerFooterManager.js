4af5c6fd1b89cfde31149e7cb33b35e58331428a
joshuakiggundu <kiggundujoshua256@gmail.com>
2026-08-22 16:04:38 +0300

    rain

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -10,7 +10,7 @@ class SpecialHeader extends HTMLElement {
                                 <div class="header-widget d-flex">
                                     <div class="topbar-widget"><a href="#"><i class="icofont-phone"></i>+256 701 430 671</a></div>
                                     <div class="topbar-widget"><a href="#"><i class="icofont-phone"></i>+256 776 430 671</a></div>
-                                    <div class="topbar-widget"><a href="#"><i class="icofont-envelope"></i>info@airoptionsuganda.com</a></div>
+                                    <div class="topbar-widget"><a href="#"><i class="icofont-envelope"></i>info@airoptionsug.com</a></div>
                                     <div class="topbar-widget"><a href="#"><i class="icofont-wall-clock"></i>Mon - Fri 08.00am - 17.00pm</a></div>
                                 </div>
 
