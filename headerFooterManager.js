b0f97f6c8955986a15bca75c4ba0ff3432a6d866
joshuakiggundu <kiggundujoshua256@gmail.com>
2026-08-22 16:05:53 +0300

    fit

--- headerFooterManager.js
+++ headerFooterManager.js
@@ -136,7 +136,7 @@ class SpecialFooter extends HTMLElement {
                             <div class="spacer-20"></div>
 
                             <div class="fw-bold text-white"><i class="icofont-envelope me-2 id-color-2"></i>Send a Message</div>
-                            info@airoptionsuganda.com                            
+                            info@airoptionsug.com                            
                         </div>
                     </div>
                 </div>
